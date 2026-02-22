03a75bdae901d1c153b902995429846071d18334
Christoph Fretter <christoph.fretter@gmail.com>
2026-02-23 00:06:15 +0800

    try to deploy in parallel

--- deploy.ps1
+++ deploy.ps1
@@ -10,7 +10,13 @@ bundle exec jekyll build
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
 Write-Host "==> Deploying to $RemoteHost via SFTP..."
-wsl lftp -e "set sftp:connect-program 'ssh -a -x'; set net:connection-limit 0; set mirror:parallel-transfer-count 10; open -u ${RemoteUser}, sftp://${RemoteHost}; mirror -R --delete --verbose --parallel=10 -x '\.jpg$' _site/ ${RemotePath}/; quit"
+wsl lftp -e "set sftp:connect-program 'ssh -a -x'; \
+set net:connection-limit 10; \
+set net:limit-max 10; \
+set net:persist-retries 2; \
+open -u ${RemoteUser}, sftp://${RemoteHost}; \
+mirror -R --delete --verbose --parallel=10 -x '\.jpg$' _site/ ${RemotePath}/; \
+quit"
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
 Write-Host "==> Done! Site live at https://paws.fretter.eu"
